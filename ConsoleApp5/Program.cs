9823db49cabf50ffac9ae66aefd0b34aa5bea63f
Игорь Некрашевич <nekrashevich@itstep.kz>
2019-12-07 19:35:32 +0700

    варианты вывода на консоль

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();//что бы считать массив в консоле
+            foreach(int el in arr)
+            {
+                Console.Write(el + " ");
+            }
+        }
+    }
+}
